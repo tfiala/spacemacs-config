7f6b31edc3279f81a2b0de48c335fe523f522803
Todd Fiala <todd.fiala@gmail.com>
2016-08-31 00:32:08 -0700

    really check for emacsclient existence
    Add an existence check for the computed emacsclient path before
    setting the variable that indicates the path.
    
    Also, remove the message calls that were commented out.

--- layers/emacs-app-bundle/packages.el
+++ layers/emacs-app-bundle/packages.el
@@ -8,20 +8,18 @@
 (defun emacs-app-bundle/set-emacsclient-path-as-needed ()
   ;; We only care when on macOS and the Emacs executable
   ;; is within a directory that ends in .app.
-  ;; (message "Running set-meacsclient-path-as-needed.")
   (when (and (eq system-type 'darwin)
              (string-match ".app/" invocation-directory))
-    ;; (message "We're on macOS and Emacs appears to be an app bundle.")
     ;; At https://emacsformacosx.com, the Emacs.app bundles
     ;; have an Emacs binary with a name like
     ;; Emacs-x86_64-10_9.  The relevant emacsclient is
     ;; then in a directory below the Emacs binary with
     ;; a name like bin-x86_64-10_9.
     (when (string-match "^Emacs\\(.+\\)$" invocation-name)
-      ;; (message "We have pulled off the following arch path component: %s" (match-string 1 invocation-name))
       (let ((emacsclient-path (concat invocation-directory "bin" (match-string 1 invocation-name) "/emacsclient")))
         ;; If the emacsclient path we built exists, we will tell
         ;; magit about it so it knows how to call back into our Emacs
         ;; when calling out to git commands that need an editor.
-        (setq with-editor-emacsclient-executable emacsclient-path)
-        (message "Found Emacs.app emacsclient here: %s" emacsclient-path)))))
+        (when (file-exists-p emacsclient-path)
+          (setq with-editor-emacsclient-executable emacsclient-path)
+          (message "Found Emacs.app emacsclient here: %s" emacsclient-path))))))
